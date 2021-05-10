f5131d651a287d48498eaa0aea2c1338b0584bd6
Franclin <frankllin.reis@gmail.com>
2021-05-09 22:40:29 -0300

    BarChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,8 +14,6 @@ const DonutChart = () => {
 const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
 useEffect(() => {
-
-}, [])
     axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then(response => {
             const data = response.data as SaleSum[];
@@ -23,12 +21,8 @@ useEffect(() => {
             const mySeries = data.map(x => x.sum);
 
             setChartData({labels: myLabels, series: mySeries});
-        })
-
-    //  const mockData = {
-    //      series: [477138, 499928, 444867, 220426, 473088],
-    //      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //  }
+        });
+}, []);
 
     const options = {
         legend: {
